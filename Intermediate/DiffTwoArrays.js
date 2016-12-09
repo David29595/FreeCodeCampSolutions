81180d2a60c048636a4a2c31ad7fe1a7fd22fe6d
David Daly <david-daly@live.ie>
2016-12-09 22:00:34 +0000

    Add new challenge

---
+++ Intermediate/DiffTwoArrays.js
@@ -0,0 +1,14 @@
+/*
+ * Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.
+ * In other words, return the symmetric difference of the two arrays.
+ */
+
+ function diffArray(arr1, arr2) {
+  var newArr = arr1.concat(arr2);
+
+  return newArr.filter(function(item) {
+    if (arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1)  return item;
+   });
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
