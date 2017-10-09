3bbdb5920a7c4f20c08b811a69f4be63cc55936b
David Daly <david-daly@live.ie>
2017-10-09 23:45:20 +0100

    Add challenge description and update code formatting

--- Advanced/RecordCollection.js
+++ Advanced/RecordCollection.js
@@ -1,3 +1,23 @@
+/*
+    You are given a JSON object representing a part of your musical album collection. 
+    Each album has several properties and a unique id number as its key. Not all albums have complete information.
+
+    Write a function which takes an album's id (like 2548), a property prop (like "artist" or "tracks"), 
+    and a value (like "Addicted to Love") to modify the data in this collection.
+
+    If prop isn't "tracks" and value isn't empty (""), update or set the value for that record album's property.
+    Your function must always return the entire collection object.
+
+    There are several rules for handling incomplete data:
+
+    >   If prop is "tracks" but the album doesn't have a "tracks" property, create an empty array before adding 
+        the new value to the album's corresponding property.
+    
+    >   If prop is "tracks" and value isn't empty (""), push the value onto the end of the album's existing tracks array.
+        
+    >   If value is empty (""), delete the given prop property from the album.
+*/
+
 
 // Setup
 var collection = {
@@ -32,12 +52,11 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 function updateRecords(id, prop, value) {
   if(value === ""){
     delete collection[id][prop];
+  } else if (prop === 'tracks' && collection[id][prop]){
+    collection[id].tracks.push(value);
+  } else { 
+    collection[id][prop] = prop === 'tracks' ? [value]: value;
   }
-  else if (prop === 'tracks' && collection[id][prop]){
-       collection[id].tracks.push(value);
-    }
-    
-  else collection[id][prop] = prop === 'tracks' ? [value]: value;
   return collection;
 }
 
